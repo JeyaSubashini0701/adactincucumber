b6d0a8c048a174a56ee3598a6ba1dd5d51d1582f
JeyaSubashini0701 <subashiniinnaciyar@gmail.com>
2025-03-24 22:59:16 +0530

    target folder update

--- src/test/java/com/org/runnerclass/RunnerClass.java
+++ src/test/java/com/org/runnerclass/RunnerClass.java
@@ -6,11 +6,10 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\Users\\MSARUL\\eclipse-workspace"
-		+ "\\AdactinCucumber\\src\\test\\java\\com\\org\\featurefile",glue="com.org.stepdefinition",tags="@adactin",
+@CucumberOptions(features="C:\\Users\\MSARUL\\eclipse-workspace\\AdactinCucumber\\src\\test\\java\\com\\org\\featurefile\\Adactin.feature",glue="com.org.stepdefinition",tags="@adactin",
 		plugin = {
 		        "pretty", 
-		        "junit:target/cucumber-reports/Cucumber.xml"  // JUnit Report
+		        "junit:target\\cucumberreports\\Cucumber.xml"  // JUnit Report
 		    },monochrome = true)
 public class RunnerClass {
 
