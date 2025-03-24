8d27b4fae93832c72ec76807cdcbc0328e49b2b7
JeyaSubashini0701 <subashiniinnaciyar@gmail.com>
2025-03-24 22:17:46 +0530

    some changes to generate report

--- src/test/java/com/org/runnerclass/RunnerClass.java
+++ src/test/java/com/org/runnerclass/RunnerClass.java
@@ -7,13 +7,11 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="C:\\Users\\MSARUL\\eclipse-workspace"
-		+ "\\AdactinCucumber\\src\\test\\java\\com\\org\\featurefile",glue="com.org.stepdefinition",tags="@adactin",publish=true,monochrome = true,
+		+ "\\AdactinCucumber\\src\\test\\java\\com\\org\\featurefile",glue="com.org.stepdefinition",tags="@adactin",
 		plugin = {
 		        "pretty", 
-		        "html:target/cucumber-reports",  // HTML Report
-		        "json:target/cucumber-reports/Cucumber.json",  // JSON Report
 		        "junit:target/cucumber-reports/Cucumber.xml"  // JUnit Report
-		    })
+		    },monochrome = true)
 public class RunnerClass {
 
 }
