ac4039b37721a99d2869ffc020bce28462791d4f
JeyaSubashini0701 <subashiniinnaciyar@gmail.com>
2025-03-24 21:49:08 +0530

    adding plugin in runner class

--- src/test/java/com/org/runnerclass/RunnerClass.java
+++ src/test/java/com/org/runnerclass/RunnerClass.java
@@ -7,7 +7,13 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="C:\\Users\\MSARUL\\eclipse-workspace"
-		+ "\\AdactinCucumber\\src\\test\\java\\com\\org\\featurefile",glue="com.org.stepdefinition",tags="@adactin",publish=true,monochrome = true)
+		+ "\\AdactinCucumber\\src\\test\\java\\com\\org\\featurefile",glue="com.org.stepdefinition",tags="@adactin",publish=true,monochrome = true,
+		plugin = {
+		        "pretty", 
+		        "html:target/cucumber-reports",  // HTML Report
+		        "json:target/cucumber-reports/Cucumber.json",  // JSON Report
+		        "junit:target/cucumber-reports/Cucumber.xml"  // JUnit Report
+		    })
 public class RunnerClass {
 
 }
